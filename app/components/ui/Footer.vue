ea4b3082aa9154b507bdb99450378deb27a3cdbc
Raul iqbal <89615372+Rauliqbal@users.noreply.github.com>
2026-03-23 11:01:31 +0700

    style: fix footer

--- app/components/ui/Footer.vue
+++ app/components/ui/Footer.vue
@@ -1,13 +1,11 @@
 <template>
    <!-- Footer -->
-        <footer class="py-20 bg-zinc-50 dark:bg-[#0A0A0A] ">
+        <footer class="pt-20 bg-zinc-50 dark:bg-[#0A0A0A] ">
             <div class="max-w-7xl mx-auto px-6">
                 <div class="grid grid-cols-1 md:grid-cols-4 gap-12 mb-20">
                     <div class="md:col-span-1">
                         <div class="flex items-center gap-2 mb-6">
-                            <div class="w-6 h-6 bg-primary rounded-full flex items-center justify-center">
-                                <Icon name="lucide:zap" class="text-black text-xs"/>
-                            </div>
+                            <img class="h-8" src="/logo.png" alt="Nice UI">
                             <span class="text-lg font-bold tracking-tighter text-zinc-900 dark:text-white">NiceUI</span>
                         </div>
                         <p class="text-zinc-500 dark:text-gray-500 text-sm leading-relaxed">
@@ -45,13 +43,13 @@
                     </div>
                 </div>
                 
-                <div class="pt-10 border-t border-zinc-200 dark:border-white/5 flex flex-col md:flex-row justify-between items-center gap-6 text-zinc-500 dark:text-gray-500 text-xs tracking-widest uppercase">
+                <div class="py-10 border-t border-zinc-200 dark:border-white/5 flex flex-col md:flex-row justify-between items-center gap-6 text-zinc-500 dark:text-gray-500 text-xs tracking-widest uppercase">
                     <p>© 2024 NICEUI LABS. ALL RIGHTS RESERVED.</p>
                     <div class="flex items-center gap-8">
                         <span>MADE WITH <Icon name="lucide:heart" class="inline text-primary"/>BY DEVS</span>
                         <div class="flex gap-4">
-                            <a href="#" id="footer-social-1" class="hover:text-zinc-900 dark:hover:text-white transition-colors"><Icon name="lucide:github" class="text-lg"/></a>
-                            <a href="#" id="footer-social-2" class="hover:text-zinc-900 dark:hover:text-white transition-colors"><Icon name="lucide:twitter" class="text-lg"/></a>
+                            <NuxtLink to="https://github.com/Rauliqbal" target="_blank" class="hover:text-zinc-900 dark:hover:text-white transition-colors"><Icon name="lucide:github" class="text-lg"/></NuxtLink>
+                            <NuxtLink to="#" class="hover:text-zinc-900 dark:hover:text-white transition-colors"><Icon name="lucide:twitter" class="text-lg"/></NuxtLink>
                         </div>
                     </div>
                 </div>
